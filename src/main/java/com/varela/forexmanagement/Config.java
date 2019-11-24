ede1a476d4d12cecc054eddbb2594aaf5adca23a
mrkali <pedro.varela955@gmail.com>
2019-11-24 03:05:06 +0000

    Start application Add request to get list of currencies List currencies Allow pick active currencies Add icon

---
+++ src/main/java/com/varela/forexmanagement/Config.java
@@ -0,0 +1,5 @@
+package com.varela.forexmanagement;
+
+public class Config {
+    static public String apiKey = "YG5IoA09CB4r6ThMOvC1AjPEF2ORgVdxoBZPTjI1iSx6NmO";
+}
---
+++ src/main/java/com/varela/forexmanagement/api/models/CurrenciesListModel.java
@@ -0,0 +1,31 @@
+package com.varela.forexmanagement.api.models;
+
+public class CurrenciesListModel {
+    private String id;
+    private String name;
+    private String decimal;
+    private String symbol;
+
+    public CurrenciesListModel(String id, String name, String decimal, String symbol) {
+        this.id = id;
+        this.name = name;
+        this.decimal = decimal;
+        this.symbol = symbol;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDecimal() {
+        return decimal;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}
